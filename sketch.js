f1a821f596e195617f63209c50fc52ac2e6863ea
luluquinyuchin-cloud <luluquinyuchin@gmail.com>
2026-05-05 10:19:05 +0800

    2

--- sketch.js
+++ sketch.js
@@ -9,6 +9,12 @@ function setup() {
 function draw() {
   background('#e7c6ff');
 
+  // 顯示文字：水平置中，位於影像上方的背景區域
+  fill(0); // 設定文字為黑色
+  textSize(32); // 設定文字大小
+  textAlign(CENTER, CENTER);
+  text("教科123456789", width / 2, height * 0.15);
+
   // 計算影像寬高為畫布的 50%
   let vW = width * 0.5;
   let vH = height * 0.5;
